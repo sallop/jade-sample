0be02066b0be8351b17a17aaa7233b5e5d9fe6d0
sallop <shiogai1987@yahoo.co.jp>
2015-12-11 11:51:14 +0900

    add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp');
 
 var jade = require('gulp-jade');
+var browserSync = require('browser-sync').create();
+
+//gulp.task('browser-sync', function(){
+gulp.task('serve', function(){
+    browserSync.init({
+        proxy: 'www.hakobune.com/tycc_remote/jade/public'
+    });
+
+    gulp.watch("./src/*.jade", ['jade']);
+    gulp.watch("./public/*.html").on('change', browserSync.reload);
+});
 
 gulp.task('jade', function(){
     gulp.src('./src/*.jade')
@@ -14,7 +25,7 @@ gulp.task('watch', function(){
     gulp.watch('./src/*.jade', ['jade']);
 });
 
-gulp.task('default', ['jade', 'watch']);
+gulp.task('default', ['jade', 'watch', 'serve']);
 //gulp.task('default', function(){
 //    console.log('default gulp task ...');
 //});
